c0f8bb28384d5ef597f5cd33cca5c145cbfb1d0a
dhaval <dhaval@fabriclake.com>
2024-03-28 12:06:18 +0530

    dsds

--- index.js
+++ index.js
@@ -1,26 +1,24 @@
 const mysql = require('mysql');
 const express = require('express');
 const bodyParser = require('body-parser');
-const rateLimit = require('express-rate-limit');
-
 /**
  * @param {string} code The code to evaluate
  * @returns {*} The result of the evaluation
  */
 function evaluateCode(code) {
-  return eval(code); // Alert: Avoid using eval() function
-}
-
-// Example usage triggering the alert
-evaluateCode("2 + 2");
-
+    return eval(code); // Alert: Avoid using eval() function
+  }
+  
+  // Example usage triggering the alert
+  evaluateCode("2 + 2");
+  
 const app = express();
 
 // Create connection to MySQL database
 const connection = mysql.createConnection({
   host: 'localhost',
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  user: 'root',
+  password: 'password',
   database: 'fdsafdsf'
 });
 
@@ -30,26 +28,16 @@ connection.connect();
 // Middleware to parse JSON requests
 app.use(bodyParser.json());
 
-// Set up rate limiter: maximum of five requests per minute
-const limiter = rateLimit({
-  windowMs: 60 * 1000, // 1 minute
-  max: 5, // max 5 requests per windowMs
-});
-
-// Apply rate limiter to all requests
-app.use(limiter);
-
 // Endpoint to authenticate user
 app.post('/login', (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
 
-  // Fix: Use query parameters to embed user input in the SQL query
-  const query = `SELECT * FROM users WHERE username = ? AND password = ?`;
-  const queryValues = [username, password];
+  // Vulnerable SQL query susceptible to SQL injection
+  const query = `SELECT * FROM users WHERE username = '${username}' AND password = '${password}'`;
 
   // Execute the SQL query
-  connection.query(query, queryValues, (err, results) => {
+  connection.query(query, (err, results) => {
     if (err) {
       console.error('Error executing query:', err);
       return res.status(500).send('Internal Server Error');
