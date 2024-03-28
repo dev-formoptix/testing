0fb61273bd545b6dc39085d0f3c74195eeb4d867
rahulcoder010 <139946269+rahulcoder010@users.noreply.github.com>
2024-03-28 17:31:36 +0530

    Revert "FO-57 (Epic) Github CodeQl Epic (Repository: testing)"

--- index.js
+++ index.js
@@ -1,27 +1,24 @@
 const mysql = require('mysql');
 const express = require('express');
 const bodyParser = require('body-parser');
-const SqlString = require('sqlstring');
-const rateLimit = require('express-rate-limit');
-
 /**
  * @param {string} code The code to evaluate
  * @returns {*} The result of the evaluation
  */
 function evaluateCode(code) {
-  return eval(code); // Alert: Avoid using eval() function
-}
-
-// Example usage triggering the alert
-evaluateCode("2 + 2");
-
+    return eval(code); // Alert: Avoid using eval() function
+  }
+  
+  // Example usage triggering the alert
+  evaluateCode("2 + 2");
+  
 const app = express();
 
 // Create connection to MySQL database
 const connection = mysql.createConnection({
   host: 'localhost',
-  user: process.env.MYSQL_USERNAME,
-  password: process.env.MYSQL_PASSWORD,
+  user: 'root',
+  password: 'password',
   database: 'fdsafdsf'
 });
 
@@ -32,21 +29,15 @@ connection.connect();
 app.use(bodyParser.json());
 
 // Endpoint to authenticate user
-const loginLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // max 100 requests per windowMs
-});
+app.post('/login', (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
 
-app.post('/login', loginLimiter, (req, res) => {
-  const username = SqlString.escape(req.body.username);
-  const password = SqlString.escape(req.body.password);
+  // Vulnerable SQL query susceptible to SQL injection
+  const query = `SELECT * FROM users WHERE username = '${username}' AND password = '${password}'`;
 
-  // Use query parameters to prevent SQL injection
-  const query = `SELECT * FROM users WHERE username = ? AND password = ?`;
-  const values = [username, password];
-
-  // Execute the SQL query with query parameters
-  connection.query(query, values, (err, results) => {
+  // Execute the SQL query
+  connection.query(query, (err, results) => {
     if (err) {
       console.error('Error executing query:', err);
       return res.status(500).send('Internal Server Error');
