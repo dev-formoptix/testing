e1549dee159e4595d7f50ea3e69b5aa8f9f4838e
dhaval <dhaval@fabriclake.com>
2024-03-28 16:30:59 +0530

    dsadsaf

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 const mysql = require('mysql');
 const express = require('express');
 const bodyParser = require('body-parser');
-const RateLimit = require('express-rate-limit');
-
 /**
  * @param {string} code The code to evaluate
  * @returns {*} The result of the evaluation
  */
 function evaluateCode(code) {
-  return eval(code); // Alert: Avoid using eval() function
-}
-
+    return eval(code); // Alert: Avoid using eval() function
+  }
+  
+  // Example usage triggering the alert
+  evaluateCode("2 + 2");
+  
 const app = express();
-
 
 // Create connection to MySQL database
 const connection = mysql.createConnection({
   host: 'localhost',
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
+  user: 'root',
+  password: 'password',
   database: 'fdsafdsf'
 });
 
@@ -28,25 +28,16 @@ connection.connect();
 // Middleware to parse JSON requests
 app.use(bodyParser.json());
 
-// Set up rate limiter: maximum of five requests per minute
-const limiter = RateLimit({
-  windowMs: 60 * 1000, // 1 minute
-  max: 5, // max 5 requests per minute
-});
-
-// Apply rate limiter to all requests
-app.use(limiter);
-
 // Endpoint to authenticate user
 app.post('/login', (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
 
-  // GOOD: use query parameters to prevent SQL injection
-  const query = 'SELECT * FROM users WHERE username = ? AND password = ?';
+  // Vulnerable SQL query susceptible to SQL injection
+  const query = `SELECT * FROM users WHERE username = '${username}' AND password = '${password}'`;
 
-  // Execute the SQL query with query parameters
-  connection.query(query, [username, password], (err, results) => {
+  // Execute the SQL query
+  connection.query(query, (err, results) => {
     if (err) {
       console.error('Error executing query:', err);
       return res.status(500).send('Internal Server Error');
