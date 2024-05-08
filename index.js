684c7d3663c0ac4a218a752fc9a72e2c5453d679
rahulcoder010 <139946269+rahulcoder010@users.noreply.github.com>
2024-05-08 17:12:20 +0530

    HT-60 (task) Test

--- index.js
+++ index.js
@@ -1,23 +1,14 @@
 const mysql = require('mysql');
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const rateLimit = require("express-rate-limit");
 
-/**
- * @param {string} code The code to evaluate
- * @returns {*} The result of the evaluation
- */
 function evaluateCode(code) {
-  return eval(code); // Alert: Avoid using eval() function
+  return Function(`"use strict";return (${code})`)();
 }
-
-// Example usage triggering the alert
-evaluateCode("2 + 2");
 
 const app = express();
 
-// Create connection to MySQL database
 const connection = mysql.createConnection({
   host: 'localhost',
   user: process.env.DB_USERNAME,
@@ -25,31 +16,25 @@ const connection = mysql.createConnection({
   database: 'sasads'
 });
 
-// Connect to MySQL database
 connection.connect();
 
-// Middleware to parse JSON requests
 app.use(bodyParser.json());
 
-// Create rate limiter
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // max 100 requests per windowMs
+  windowMs: 15 * 60 * 1000,
+  max: 100,
 });
 
-// Apply rate limiter to all requests
 app.use(limiter);
 
-// Endpoint to authenticate user
 app.post('/login', (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
 
-  // Vulnerable SQL query susceptible to SQL injection
-  const query = `SELECT * FROM users WHERE username = '${username}' AND password = '${password}'`;
+  const query = `SELECT * FROM users WHERE username = ? AND password = ?`;
+  const values = [username, password];
 
-  // Execute the SQL query
-  connection.query(query, (err, results) => {
+  connection.query(query, values, (err, results) => {
     if (err) {
       console.error('Error executing query:', err);
       return res.status(500).send('Internal Server Error');
@@ -63,7 +48,6 @@ app.post('/login', (req, res) => {
   });
 });
 
-// Start the server
 const port = 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
